0330fb89d200ae70d00f38549a0d2635aaa2bc18
Edward <satori.tendo.00@mail.ru>
2023-06-24 13:28:50 +0300

    UI without adaptive

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import s from './App.module.scss';
+import arrow from './assets/arrow.gif'
+import beforeimg from './assets/beforeimg.jpg'
+import afterimg from './assets/afterimg.png'
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.app}>
+      <div className={s.container}>
+        <h1>Здесь ты можешь сделать пиксельную картинку из обычной</h1>
+        <div className={s.images}>
+          <img src={beforeimg} alt='' className={`${s.image1} ${s.image}`} />
+          <img src={arrow} alt="" className={s.arrow}/>
+          <img src={afterimg} alt='' className={`${s.image2} ${s.image}`} />
+        </div>
+        <div className={s.buttonWrapper}>
+          <button className={s.button}>Сделать картинку</button>
+        </div>
+      </div>
     </div>
   );
 }
